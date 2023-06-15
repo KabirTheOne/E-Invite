bb57e54c75d06b998ca48aa1777baf8885d9154e
Kabir <96368363+KabirTheOne@users.noreply.github.com>
2023-06-15 20:45:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+/*window.onload = function() {
+    if (localStorage.getItem("hasCodeRunBefore") === null) {
+        document.getElementById("Bday_audio").play();
+        localStorage.setItem("hasCodeRunBefore", true);
+    }
+}
+function playsound() {
+    document.getElementById("Bday_audio").play();
+}*/
